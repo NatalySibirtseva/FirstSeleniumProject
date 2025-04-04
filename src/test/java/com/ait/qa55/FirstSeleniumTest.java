f50501c4311ff5f23c8f43e097e7836dd5c0ce6d
natal <nataly.sib@gmail.com>
2025-04-04 11:01:24 +0200

    Lesson14

--- src/test/java/com/ait/qa55/FirstSeleniumTest.java
+++ src/test/java/com/ait/qa55/FirstSeleniumTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class FirstSeleniumTest {
 
     WebDriver driver;
@@ -13,11 +15,22 @@ public class FirstSeleniumTest {
     //before - setUp
     @BeforeMethod
     public void setUp() {
+
         driver = new ChromeDriver();
+
         //driver.get("https://www.google.com"); //without history, как новый/чистый пользователь пришел
+
+        //maximize browser to window, растянуть на все окно
+        driver.manage().window().maximize();
+
         driver.navigate().to("https://seferisrael.co.il"); //with history
+        //wait for all elements on the site to load before starting test
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //implicitlyWait - неявное ожидание
+
         driver.navigate().to("https://www.google.com");
         driver.navigate().back();
+        driver.navigate().refresh(); //перезагрузка страницы
+
     }
 
 
@@ -32,7 +45,8 @@ public class FirstSeleniumTest {
     //(enabled = false) - стоит чтобы не закрывался пока браузер, пока мы отладку делаем
     @AfterMethod(enabled = false)
     public void tearDown() {
-        driver.quit();
+        //driver.quit(); //all tabs and brauser - закрывает все вкладки и сам браузер,если они были открыты в процессе теста
+        driver.close(); // only one tab - закрывает только текущую вкладку, если она одна - закроет и браузер
     }
 
 
